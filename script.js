e3e4960a14fd53e14629462a7af63694f549e89b
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-07 20:22:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let userName = prompt("What is your name?");
+let favoriteFood = prompt("What is your favorite food?");
+
+console.log("Hello " + userName + "! " + favoriteFood + " sounds delicious!");
